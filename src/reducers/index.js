c8db5c4f49340b39d12aa6921139d9f8cad66d49
leemuro <lee@leemuro.com>
2017-03-08 05:29:33 -0500

    Refactors reducer to not use object spread
    object spreads have poor compatibility i guess, and this just looks
    cleaner.  I also added some whitespace in the reducer to format it more
    nicely.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -23,42 +23,48 @@ export default function appReducer(state = initialState, action) {
       return Object.assign({}, state, {
         screen: LIST_SCREEN
       })
+      
     case SHOW_ADD:
       return Object.assign({}, state, {
         screen: ADD_SCREEN
       })
+      
     case ADD_ITEMS: 
       let newRawItems = action.newItems.reduce((obj, i) => {
         obj[Gun.text.random()] = { description: i, completed: false }
         return obj
       }, state.rawItems)
+      
       return Object.assign({}, state, {
         rawItems: newRawItems,
         categorizedItems: categorizeItems(newRawItems),
         screen: LIST_SCREEN
       })
+      
     case CLEAR_ITEMS:
       return Object.assign({}, state, {
         rawItems: {},
         categorizedItems: {}
       })
+      
     case TOGGLE_ITEM:
+      let rawItem = state.rawItems[action.itemId]
+      let category = state.categorizedItems[action.categoryName]
+      let categoryItem = category[action.itemId]
+      
       return Object.assign({}, state, {
-        rawItems: {
-          ...state.rawItems,
-          [action.itemId]: Object.assign({}, state.rawItems[action.itemId], { 
-            completed: !state.rawItems[action.itemId].completed 
+        rawItems: Object.assign({}, state.rawItems, {
+          [action.itemId]: Object.assign({}, rawItem, { 
+            completed: !rawItem.completed
           })
-        },
-        categorizedItems: {
-          ...state.categorizedItems,
-          [action.categoryName]: {
-            ...state.categorizedItems[action.categoryName],
-            [action.itemId]: Object.assign({}, state.categorizedItems[action.categoryName][action.itemId], { 
-              completed: !state.categorizedItems[action.categoryName][action.itemId].completed 
+        }),
+        categorizedItems: Object.assign({}, state.categorizedItems, {
+          [action.categoryName]: Object.assign({}, category, {
+            [action.itemId]: Object.assign({}, categoryItem, { 
+              completed: !categoryItem.completed 
             })
-          }
-        }
+          })
+        })
       })
     default:
       return state
